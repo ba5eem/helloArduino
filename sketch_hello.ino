b76627add74a3a6d3376f9c5b78d3dfad1041ab9
ba5eem <baseem@gmail.com>
2018-06-29 12:02:37 -1000

    project update

--- sketch_hello.ino
+++ sketch_hello.ino
@@ -1,35 +1,21 @@
-/*
-Adafruit Arduino - Lesson 5. Serial Monitor
-*/
-
-char ch;
-int i = 1;
-
 void setup()
 {
   Serial.begin(9600);
   while (! Serial); // Wait untilSerial is ready - Leonardo
-  Serial.println("Enter number from 0 - 10");
+  Serial.println("Enter LED Number 0 to 7 or 'x' to clear");
 }
 
 void loop()
 {
   if (Serial.available())
   {
-    ch = Serial.read();
-  };
-  if(ch == '1'){
-    while (i == 1) {
-      Serial.print("UNO");
-      i++;
+    char ch = Serial.read();
+    if (ch)
+    {
+      int led = ch - '0';
+      Serial.print("You Chose ");
+      Serial.print(led);
     }
-  };
-  if(ch == '2'){
-    while (i == 1) {
-      Serial.print("DOUS");
-      i++;
-    }
-  };
-
-
+  }
 }
+
